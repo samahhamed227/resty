50123da0f6d679955b82ffc44788ad1c12780ba5
samahhamed227 <samahhamed227@gmail.com>
2021-12-05 17:14:06 +0200

    done

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./header.scss";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header>
-        <h1>RESTy</h1>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header>
+      <h1 className="h1">REST Y</h1>
+    </header>
+  );
 }
 
 export default Header;
